a69537efd06251d5357d12003a85345ae3d7920a
WinnieQueen <ashilliard@student.neumont.edu>
2020-04-22 07:30:58 -0600

    started code, fixed UML

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -3,26 +3,59 @@ import java.util.HashMap;
 
 public class Program {
 
-    private HashMap<String, String> schema = new HashMap<>();
-    private File file;
+    private static HashMap<String, String> schema = new HashMap<>();
+    private static File file;
+    private static Boolean tableCreated = false;
 
     public static void main(String[] args) {
         run();
     }
 
     public static void run() {
-
+        Boolean valid = false;
+        if (tableCreated) {
+            valid = false;
+            do {
+                System.out.println("Enter a search:");
+                String result = promptForInput();
+                searchFile(result);
+                if (!createSchema(result)) {
+                    System.out.println("Sorry, something went wrong while processing your search. Please try again.");
+                    valid = false;
+                } else {
+                    valid = true;
+                }
+            } while (!valid);
+        } else {
+            valid = false;
+            if (tableCreated) {
+                do {
+                    System.out.println("Please create your schema:");
+                    String result = promptForInput();
+                    createSchema(result);
+                    if (!createSchema(result)) {
+                        System.out.println("Sorry, something went wrong while creating the table. Please try again.");
+                        valid = false;
+                    } else {
+                        valid = true;
+                    }
+                } while (!valid);
+            }
+        }
     }
 
-    public static String promptForInput() {
-        return null;
-    }
+        public static String promptForInput () {
 
-    public static Boolean createSchema() {
-        return false;
-    }
+            return null;
+        }
 
-    public static String searchFile() {
-        return null;
+        public static Boolean createSchema (String input){
+
+            return false;
+        }
+
+        public static Boolean searchFile (String input) {
+
+            return null;
+        }
     }
-}
