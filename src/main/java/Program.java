b18817921351bc45b477ac0bcc6d529c14d5eda9
WinnieQueen <ashilliard@student.neumont.edu>
2020-04-22 05:19:12 -0600

    program and test creation

---
+++ src/main/java/Program.java
@@ -0,0 +1,28 @@
+import java.io.File;
+import java.util.HashMap;
+
+public class Program {
+
+    private HashMap<String, String> schema = new HashMap<>();
+    private File file;
+
+    public static void main(String[] args) {
+        run();
+    }
+
+    public static void run() {
+
+    }
+
+    public static String promptForInput() {
+        return null;
+    }
+
+    public static Boolean createSchema() {
+        return false;
+    }
+
+    public static String searchFile() {
+        return null;
+    }
+}
---
+++ src/test/java/ProgramTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProgramTest {
+
+    @Test
+    void promptForInput() {
+    }
+
+    @Test
+    void createSchema() {
+    }
+
+    @Test
+    void searchFile() {
+    }
+}
